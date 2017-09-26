b6c4dedbd5bb301358d5854852024942fee5a4c4
Johnathan Chen <jc.swfit@icloud.com>
2017-09-26 12:32:11 -0700

    AR Ghost Finished

---
+++ Ghost/Ghost/Scene.swift
@@ -0,0 +1,101 @@
+//
+//  Scene.swift
+//  Ghost
+//
+//  Created by Johnathan Chen on 9/26/17.
+//  Copyright © 2017 JCSwifty. All rights reserved.
+//
+
+import SpriteKit
+import ARKit
+
+class Scene: SKScene {
+    
+    let ghostsLabel = SKLabelNode(text: "Ghosts")
+    let numberOfGhostsLabel = SKLabelNode(text: "0")
+    var creationTime : TimeInterval = 0
+    var ghostCount = 0 {
+        didSet {
+            self.numberOfGhostsLabel.text = "\(ghostCount)"
+        }
+    }
+    
+    let killSound = SKAction.playSoundFileNamed("ghost", waitForCompletion: false)
+    
+    override func didMove(to view: SKView) {
+        ghostsLabel.fontSize = 20
+        ghostsLabel.fontName = "DevanagariSangamMN-Bold"
+        ghostsLabel.color = .white
+        ghostsLabel.position = CGPoint(x: 40, y: 50)
+        addChild(ghostsLabel)
+        
+        numberOfGhostsLabel.fontSize = 30
+        numberOfGhostsLabel.fontSize = 20
+        numberOfGhostsLabel.fontName = "DevanagariSangamMN-Bold"
+        numberOfGhostsLabel.color = .white
+        numberOfGhostsLabel.position = CGPoint(x: 40, y: 10)
+        addChild(numberOfGhostsLabel)
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        if currentTime > creationTime {
+            createGhostAnchor()
+            creationTime = currentTime + TimeInterval(randomFloat(min: 3.0, max: 6.0))
+        }
+    }
+    
+    func randomFloat(min: Float, max: Float) -> Float {
+        return (Float(arc4random()) / 0xFFFFFFFF) * (max - min) + min
+    }
+    
+    func createGhostAnchor() {
+        guard let sceneView = self.view as? ARSKView else {
+            return
+        }
+        let _360degrees = 2.0 * Float.pi
+        
+        let rotateX = simd_float4x4(SCNMatrix4MakeRotation(_360degrees * randomFloat(min: 0.0, max: 1.0), 1, 0, 0))
+        
+        let rotateY = simd_float4x4(SCNMatrix4MakeRotation(_360degrees * randomFloat(min: 0.0, max: 1.0), 0, 1, 0))
+        
+        let rotation = simd_mul(rotateX, rotateY)
+        
+        var translation = matrix_identity_float4x4
+        translation.columns.3.z = -1 - randomFloat(min: 0.0, max: 1.0)
+        
+        let transform = simd_mul(rotation, translation)
+        
+        let anchor = ARAnchor(transform: transform)
+        sceneView.session.add(anchor: anchor)
+        
+        ghostCount += 1
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {
+            return
+        }
+        let location = touch.location(in: self)
+     
+        let hit = nodes(at: location)
+        
+        if let node = hit.first {
+            if node.name == "ghost" {
+                
+                let fadeOut = SKAction.fadeOut(withDuration: 0.5)
+                let remove = SKAction.removeFromParent()
+                
+                // Group the fade out and sound actions
+                let groupKillingActions = SKAction.group([fadeOut, killSound])
+                // Create an action sequence
+                let sequenceAction = SKAction.sequence([groupKillingActions, remove])
+                
+                node.run(sequenceAction)
+                ghostCount -= 1
+            }
+        }
+    }
+}
+
+
---
+++ Ghost/Ghost/ViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ViewController.swift
+//  Ghost
+//
+//  Created by Johnathan Chen on 9/26/17.
+//  Copyright © 2017 JCSwifty. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSKViewDelegate {
+
+    @IBOutlet var sceneView: ARSKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsFPS = true
+        sceneView.showsNodeCount = true
+        
+        let scene = Scene(size: sceneView.bounds.size)
+        scene.scaleMode = .resizeFill
+        sceneView.presentScene(scene)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    // MARK: - ARSKViewDelegate
+    
+    func randomInt(min: Int, max: Int) -> Int {
+        return min + Int(arc4random_uniform(UInt32(max - min + 1)))
+    }
+    
+    func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
+        let ghostId = randomInt(min: 1, max: 6)
+        let node = SKSpriteNode(imageNamed: "ghost\(ghostId)")
+        node.name = "ghost"
+        
+        return node
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
